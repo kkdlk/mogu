0b72308321818a9c28e91585348b79dd01fe894b
kkdaj <1161493927@qq.com>
2020-12-03 09:53:14 +0800

    更新专业学前教育

--- src/components/contextText.js
+++ src/components/contextText.js
@@ -8,8 +8,7 @@
 
 let huli = require("../context/huli.json")
 let dianzixinxi = require("../context/dianzixinxi.json")
-
-
+let xueqianjiaoyu = require("../context/xueqianjiaoyuDay.json")
 
 // 日报内容生成
 /**
@@ -45,6 +44,17 @@ function contentTxts (config,iterNum){
           }
           if(iterNum==2){console.log("日报内容生成SUCCESS")}else if(iterNum==3){console.log("周报内容生成SUCCESS")}else if(iterNum==4){console.log("月报内容生成SUCCESS");}
           return texts;
+      }else if (college=="学前教育"){
+        var result  = xueqianjiaoyu.data
+        var texts = "";
+        let reslength = result.length
+        for (let index = 0; index < iterNum; index++) {
+          let resultRandomLength = Math.round(Math.random()*reslength) // 从0~数据长度 角标
+          texts += result[resultRandomLength].txt;
+          texts += ";";
+        }
+        if(iterNum==2){console.log("日报内容生成SUCCESS")}else if(iterNum==3){console.log("周报内容生成SUCCESS")}else if(iterNum==4){console.log("月报内容生成SUCCESS");}
+        return texts;
       }
       console.log("没有内置这个专业")
       return "";
